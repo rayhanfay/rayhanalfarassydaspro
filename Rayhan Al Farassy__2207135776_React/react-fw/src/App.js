66f61f4ca0db44996b22a51ec2053f643230da8b
al <114874795+rayhanfay@users.noreply.github.com>
2022-12-14 17:59:12 +0700

    Add files via upload

---
+++ Rayhan Al Farassy__2207135776_React/react-fw/src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import logo from './logoo.png';
+import './App.css';
+import User from './components/User';
+import Blog from './components/Blog';
+import Account from './components/Account';
+
+
+class App extends Component{
+  render() {
+    return (
+      <Router>
+      <div>
+          <Link to="/">|Home|</Link>
+          <Link to="/account">|Account|</Link>
+          <Link to="/blog">|Blog|</Link>
+        <hr/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Routes>
+        <Route exact path="/" element={<User/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+        <Route path="/account" element={<Account/>}/>
+        </Routes>
+      </div>
+      </Router>
+      
+    );
+  }  
+}
+
+export default App;
